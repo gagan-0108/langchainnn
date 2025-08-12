4c22865bde6a37877c8fda4dbcfdd8e45752a33a
Gagan Deep Yadav <yadavgagan61@gmail.com>
2025-08-12 17:01:34 +0530

    some changes

--- 5.Output/with_structured_output_typeddict.py
+++ 5.Output/with_structured_output_typeddict.py
@@ -7,13 +7,12 @@ load_dotenv()
 model = ChatGoogleGenerativeAI(model = "gemini-2.5-flash")
 class Review(TypedDict):
     key_themes: Annotated [list[str], "Write down all the key point from the review"]
-    summary: Annotated [Literal['pros', 'negative'], "A brief summary of a review" ]
-    sentiment: Annotated[str,"Return Sentiment of the review"]
+    summary: Annotated [str, "A brief summary of a review" ]
+    sentiment: Annotated[Literal['pros', 'negative'],"Return Sentiment of the review"]
     pros: Annotated[Optional[list[str]], "Write down all the pros inside tk"] 
     cons: Annotated[Optional[list[str]], "Write down all the cons inside tk"] 
 
 StructuredModel = model.with_structured_output(Review)
-
 
 result = StructuredModel.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
 
