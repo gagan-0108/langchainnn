7477cc2d127f8b165c8573c052f73c4a38b6d4f8
Gagan Deep Yadav <yadavgagan61@gmail.com>
2025-08-25 12:09:03 +0530

    lllm chain

---
+++ 8.Runnables/llm_chain.py
@@ -0,0 +1,6 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='/Users/gagan/Desktop/langchainnnn/.env')
