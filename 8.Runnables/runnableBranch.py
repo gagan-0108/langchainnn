2b03437bbd86c98a7413e9b75dfc32536ab0f158
Gagan Deep Yadav <yadavgagan61@gmail.com>
2025-08-26 16:51:00 +0530

    runnable branch complete

--- 8.Runnables/runnableBranch.py
+++ 8.Runnables/runnableBranch.py
@@ -1,7 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain.schema.runnable import RunnablePassthrough , RunnableParallel, RunnableSequence
+from langchain.schema.runnable import RunnablePassthrough , RunnableParallel, RunnableSequence, RunnableBranch
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="/Users/gagan/Desktop/langchainnnn/.env")
@@ -20,3 +20,15 @@ prompt2 = PromptTemplate(
     input_variables=['text']
 )
 
+report_generate =  RunnableSequence(prompt1, model , parser)
+
+branch_chain = RunnableBranch(
+    ( lambda x: len(x.split())>100 , RunnableSequence(prompt2, model , parser) ),
+    RunnablePassthrough()
+)
+
+final_chain = RunnableSequence(report_generate, branch_chain)
+
+result = final_chain.invoke({'topic': 'pollution in india'})
+
+print( result)
